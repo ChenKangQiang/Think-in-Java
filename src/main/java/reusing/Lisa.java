93e07d8be9b0a97da2c5dd65dddab7dcad22b8dd
ChenKangQiang <ckq123@outlook.com>
2017-06-29 19:52:47 +0800

    add files

---
+++ src/main/java/reusing/Lisa.java
@@ -0,0 +1,22 @@
+//: reusing/Lisa.java
+package reusing; /* Added by Eclipse.py */
+// {CompileTimeError} (Won't compile)
+
+class Lisa extends Homer {
+
+	@Override
+	char doh(char c) {
+		// TODO Auto-generated method stub
+		return super.doh(c);
+	}
+
+	@Override
+	float doh(float f) {
+		// TODO Auto-generated method stub
+		return super.doh(f);
+	}
+	
+//  @Override void doh(Milhouse m) {
+//    System.out.println("doh(Milhouse)");
+//  }
+} ///:~
